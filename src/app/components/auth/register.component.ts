cb16e717e89e79d0856c0c7f73dbdae57ed100c2
AnkurRam2002 <ankurram2002@gmail.com>
2025-06-19 17:37:09 +0530

    view note added

--- src/app/components/auth/register.component.ts
+++ src/app/components/auth/register.component.ts
@@ -18,9 +18,28 @@ export class RegisterComponent {
   constructor(private auth: AuthService, private router: Router) {}
 
   register() {
+    if (!this.validateEmail(this.email)) {
+    alert('Please enter a valid email address.');
+    return;
+  }
+   if (!this.validatePassword(this.password)) {
+    alert('Password must be at least 5 characters long.');
+    return;
+  }
     this.auth.register(this.name, this.email, this.password).subscribe({
       next: () => this.router.navigate(['/login']),
       error: err => alert('Registration failed')
     });
   }
+
+  validateEmail(email: string): boolean {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+validatePassword(password: string): boolean {
+  const passwordRegex = /^.{5,}$/;
+  return passwordRegex.test(password);
+}
+
 }
